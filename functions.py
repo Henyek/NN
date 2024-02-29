07af2bc452465d332d84c11ebcb0f7e365973a5b
Henyek <102858694+Henyek@users.noreply.github.com>
2024-02-29 18:14:58 +0100

    Image Generation NN

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle as pkl
 import pygame
-import cupy as cp
+# import cupy as cp
 
 
 
@@ -9,8 +9,8 @@ def RElU(x):
     return np.maximum(0.1*x, x)
 
 
-def RElU_CUDA(x):
-    return cp.maximum(0.1*x, x)
+# def RElU_CUDA(x):
+#     return cp.maximum(0.1*x, x)
 
 def RElU_deriv(x):
     return x > 0
@@ -18,8 +18,8 @@ def RElU_deriv(x):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def sigmoid_CUDA(x):
-    return 1 / (1 + cp.exp(-x))
+# def sigmoid_CUDA(x):
+#     return 1 / (1 + cp.exp(-x))
 
 def sigmoid_deriv(x):
     return sigmoid(x) * (1 - sigmoid(x))
@@ -35,15 +35,15 @@ class Network:
         self.W3 = np.random.rand(geometry[3],geometry[2])*2-1
         self.B3 = np.random.rand(geometry[3],1)*2-1
         
-    def init_CUDA(self, geometry):
-        self.W1 = cp.random.rand(geometry[1], geometry[0])*2-1
-        self.Z1, self.Z2, self.Z3 = [], [], []
-        self.A1, self.A2, self.A3 = [], [], []
-        self.B1 = cp.random.rand(geometry[1],1)*2-1
-        self.W2 = cp.random.rand(geometry[2],geometry[1])*2-1
-        self.B2 = cp.random.rand(geometry[2],1)*2-1
-        self.W3 = cp.random.rand(geometry[3],geometry[2])*2-1
-        self.B3 = cp.random.rand(geometry[3],1)*2-1
+    # def init_CUDA(self, geometry):
+    #     self.W1 = cp.random.rand(geometry[1], geometry[0])*2-1
+    #     self.Z1, self.Z2, self.Z3 = [], [], []
+    #     self.A1, self.A2, self.A3 = [], [], []
+    #     self.B1 = cp.random.rand(geometry[1],1)*2-1
+    #     self.W2 = cp.random.rand(geometry[2],geometry[1])*2-1
+    #     self.B2 = cp.random.rand(geometry[2],1)*2-1
+    #     self.W3 = cp.random.rand(geometry[3],geometry[2])*2-1
+    #     self.B3 = cp.random.rand(geometry[3],1)*2-1
 
     def prnt(self):
         print(self.W1, self.B1, self.W2, self.B2, self.W3, self.B3)
@@ -57,18 +57,19 @@ class Network:
         self.A3 = sigmoid(self.Z3)
         return self.A3
     
-    def Fprop_CUDA(self, inputs):
-        self.Z1 = cp.add(cp.dot(self.W1, inputs), self.B1)
-        self.A1 = sigmoid(self.Z1)
-        self.Z2 = cp.add(cp.dot(self.W2, self.A1), self.B2)
-        self.A2 = RElU(self.Z2)
-        self.Z3 = cp.add(cp.dot(self.W3, self.A2), self.B3)
-        self.A3 = sigmoid(self.Z3)
-        return self.A3
+    # def Fprop_CUDA(self, inputs):
+    #     self.Z1 = cp.add(cp.dot(self.W1, inputs), self.B1)
+    #     self.A1 = sigmoid(self.Z1)
+    #     self.Z2 = cp.add(cp.dot(self.W2, self.A1), self.B2)
+    #     self.A2 = RElU(self.Z2)
+    #     self.Z3 = cp.add(cp.dot(self.W3, self.A2), self.B3)
+    #     self.A3 = sigmoid(self.Z3)
+    #     return self.A3
     
     def Bprop(self, labels, inputs, shape):
-        expected = np.zeros((labels.size, 10))
-        expected[np.arange(labels.size), labels] = 1
+        # expected = np.zeros((labels.size, 10))
+        # expected[np.arange(labels.size), labels] = 1
+        expected = labels
         dZ3 = np.subtract(self.A3, expected.T)
         dW3 = (1 / shape[0]) * dZ3.dot(self.A2.T)
         dB3 = (1 / shape[0]) * np.sum(dZ3)
@@ -80,19 +81,19 @@ class Network:
         dB1 = (1 / shape[0]) * np.sum(dZ1)
         return(dW3, dB3, dW2, dB2, dW1, dB1)
     
-    def Bprop_CUDA(self, labels, inputs, shape):
-        expected = cp.zeros((labels.size, 10))
-        expected[cp.arange(labels.size), labels] = 1
-        dZ3 = cp.subtract(self.A3, expected.T)
-        dW3 = (1 / shape[0]) * cp.dot(dZ3, self.A2.T)
-        dB3 = (1 / shape[0]) * cp.sum(dZ3)
-        dZ2 = cp.dot(self.W3.T, dZ3) * RElU_deriv(self.Z2)
-        dW2 = (1 / shape[0]) * cp.dot(dZ2, self.A1.T)
-        dB2 = (1 / shape[0]) * cp.sum(dZ2)
-        dZ1 = cp.dot(self.W2.T, dZ2) * sigmoid_deriv(self.Z1)
-        dW1 = (1 / shape[0]) * cp.dot(dZ1, inputs.T)
-        dB1 = (1 / shape[0]) * cp.sum(dZ1)
-        return(dW3, dB3, dW2, dB2, dW1, dB1)
+    # def Bprop_CUDA(self, labels, inputs, shape):
+    #     expected = cp.zeros((labels.size, 10))
+    #     expected[cp.arange(labels.size), labels] = 1
+    #     dZ3 = cp.subtract(self.A3, expected.T)
+    #     dW3 = (1 / shape[0]) * cp.dot(dZ3, self.A2.T)
+    #     dB3 = (1 / shape[0]) * cp.sum(dZ3)
+    #     dZ2 = cp.dot(self.W3.T, dZ3) * RElU_deriv(self.Z2)
+    #     dW2 = (1 / shape[0]) * cp.dot(dZ2, self.A1.T)
+    #     dB2 = (1 / shape[0]) * cp.sum(dZ2)
+    #     dZ1 = cp.dot(self.W2.T, dZ2) * sigmoid_deriv(self.Z1)
+    #     dW1 = (1 / shape[0]) * cp.dot(dZ1, inputs.T)
+    #     dB1 = (1 / shape[0]) * cp.sum(dZ1)
+    #     return(dW3, dB3, dW2, dB2, dW1, dB1)
     
     def update_params(self, dW3, dB3, dW2, dB2, dW1, dB1, alpha):
         self.W1 = self.W1 - alpha * dW1
@@ -105,14 +106,14 @@ class Network:
     def get_perdict(self):
         return np.argmax(self.A3, 0)
     
-    def get_perdict_CUDA(self):
-        return cp.argmax(self.A3, 0)
+    # def get_perdict_CUDA(self):
+    #     return cp.argmax(self.A3, 0)
     
     def get_accu(self, predictions, labels):
         return np.sum(predictions == labels) / labels.size    
     
-    def get_accu_CUDA(self, predictions, labels):
-        return cp.sum(predictions == labels) / labels.size
+    # def get_accu_CUDA(self, predictions, labels):
+    #     return cp.sum(predictions == labels) / labels.size
         
     def save_network(self):
         with open('network_data.pickle', 'wb') as file:
@@ -135,16 +136,19 @@ class Network:
 def draw_image(SCREEN, img, pos):
     temp1, temp2 = 0, 0
     rects = []
-    for x in range(len(img)):
-        color = img[x]*255
-        if(x % 28 == 0):
+    for x in range(len(img) - 2048):
+        colorR = int(img[x]*255)
+        colorG = int(img[x+1024]*255)
+        colorB = int(img[x+2048]*255)
+
+        if(x % 32 == 0):
             temp1 = 0
             temp2 += 1
         else:
             temp1 += 1
         rect = pygame.Rect(pos[0]+(temp1*10),pos[1]+(temp2*10), 10, 10)
         rects.append(rect)
-        pygame.draw.rect(SCREEN, (color, color, color), rect)
+        pygame.draw.rect(SCREEN, (colorR, colorG, colorB), rect)
     return rects
 
 def editor(rects, img):
@@ -154,13 +158,19 @@ def editor(rects, img):
                 img[rect_index] = 1
 
             if img[rect_index-1] <= 0.9:
-                img[rect_index-1] += 0.05
+                img[rect_index-1] += 0.01
 
             if img[rect_index+1] <= 0.9:
-                img[rect_index+1] += 0.05
+                img[rect_index+1] += 0.01
 
             if img[rect_index-28] <= 0.9:
-                img[rect_index-28] += 0.05
+                img[rect_index-28] += 0.01
 
             if img[rect_index+28] <= 0.9:
-                img[rect_index+28] += 0.05
+                img[rect_index+28] += 0.01
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
--- nn.py
+++ nn.py
@@ -2,12 +2,12 @@ from functions import *
 import pandas as pd
 from matplotlib import pyplot
 import time
-import cupy as cp
+# import cupy as cp
 
-geometry = [784, 128, 64, 10]
+geometry = [10, 256, 1028, 3072]
 
 NN = Network(geometry)
-NN.init_CUDA(geometry)
+# NN.init_CUDA(geometry)
 
 pygame.init()
 
@@ -15,22 +15,48 @@ SCREEN = pygame.display.set_mode((640, 640))
 
 # dataset = pd.read_csv(r'mnist_train.csv', nrows=60000)
 
-# data = cp.array(dataset)
+# batch_label, labels, data, filenames
+
+data1 = unpickle(f"data_batch_1")
+data2 = unpickle(f"data_batch_2")
+data3 = unpickle(f"data_batch_3")
+data4 = unpickle(f"data_batch_4")
+data5 = unpickle(f"data_batch_5")
+
+labels = np.concatenate([data1[b'data'], data2[b'data'], data3[b'data'], data4[b'data'], data5[b'data']])/255
+inputs_temp = np.concatenate([data1[b'labels'], data2[b'labels'], data3[b'labels'], data4[b'labels'], data5[b'labels']])
+
+# print(labels)
+
+inputs = np.zeros((inputs_temp.size, 10))
+inputs[np.arange(inputs_temp.size), inputs_temp] = 1
+
+inputs = inputs.T
+
+
+m, n = inputs.shape
+
+# print(inputs.shape)
+
+
+# data = np.array(dataset)
 
 # m, n =  data.shape
 
 # labels = data.T[0]
 # inputs = data.T[1:n]/255
 
+# print(inputs.shape)
+
 # inputs_color = data.T[1:n]
 
-# iterations = 10000
-# i = 0
-# fitness = 0
+iterations = 25
+i = 0
+fitness = 0
 
 # timer = time.time()
 
-NN.load_network(open('network_data.pickle', 'rb'))
+# NN.load_network(open('network_data_16_16.pickle', 'rb'))
 
 # for x in range(iterations):
 #     i += 1
@@ -43,18 +69,34 @@ NN.load_network(open('network_data.pickle', 'rb'))
 #         predictions = NN.get_perdict_CUDA()
 #         fitness = NN.get_accu_CUDA(predictions, labels)
 #         print(fitness)
-        
-# for x in range(iterations):
-#     i += 1
-#     NN.Fprop(inputs)
 
-#     dW3, dB3, dW2, dB2, dW1, dB1 = NN.Bprop(labels, inputs, (m, n))
-#     NN.update_params(dW3, dB3, dW2, dB2, dW1, dB1, 0.2)    
-#     if i % 20 == 0:
-#         print("Iteration: ", i)
-#         predictions = NN.get_perdict()
-#         fitness = NN.get_accu(predictions, labels)
-#         print(fitness)
+CurrentImage = []
+
+
+SCREEN.fill((255,255,255))
+pygame.display.flip()
+while True:
+    
+    SCREEN.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        
+    while i < iterations:
+        i += 1
+        NN.Fprop(inputs)
+
+        dW3, dB3, dW2, dB2, dW1, dB1 = NN.Bprop(labels, inputs, (m, n))
+        NN.update_params(dW3, dB3, dW2, dB2, dW1, dB1, 0.2)    
+        if i % 1 == 0:
+            print("Iteration: ", i)
+            # predictions = NN.get_perdict()
+            # fitness = NN.get_accu(predictions, labels)
+            # print(fitness)
+
+            CurrentImage = NN.Fprop(np.array(np.reshape([0,1,0,0,0,0,0,0,0,0], (10, 1))))
+            rects = draw_image(SCREEN, CurrentImage, (100, 100))
+            pygame.display.flip()
 
 # print(time.time() - timer)
 
@@ -77,31 +119,31 @@ NN.load_network(open('network_data.pickle', 'rb'))
 #     pyplot.show()
 #     pyplot.pause(1)  
 
-CurrentImage = [0.0] * 784
+# CurrentImage = [0.0] * 784
 
-draw_state = False
+# draw_state = False
 
-reset_button = pygame.Rect(0, 0, 200, 60)
-reset_button.centerx = SCREEN.get_rect().centerx
-reset_button.centery = SCREEN.get_rect().height - 70 
+# reset_button = pygame.Rect(0, 0, 200, 60)
+# reset_button.centerx = SCREEN.get_rect().centerx
+# reset_button.centery = SCREEN.get_rect().height - 70 
 
 
-while True:
-    SCREEN.fill((255,255,255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if reset_button.collidepoint(pygame.mouse.get_pos()):
-                CurrentImage = [0.0] * 784
-            draw_state = True
-        if event.type == pygame.MOUSEBUTTONUP:
-            draw_state = False
-    rects = draw_image(SCREEN, CurrentImage, (100, 100))
-    NN.Fprop(cp.reshape(cp.array(CurrentImage), (784, 1)))
-    print(NN.get_perdict())
+# while True:
+#     SCREEN.fill((255,255,255))
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             quit()
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             if reset_button.collidepoint(pygame.mouse.get_pos()):
+#                 CurrentImage = [0.0] * 784
+#             draw_state = True
+#         if event.type == pygame.MOUSEBUTTONUP:
+#             draw_state = False
+#     rects = draw_image(SCREEN, CurrentImage, (100, 100))
+#     NN.Fprop(np.reshape(np.array(CurrentImage), (784, 1)))
+#     print(NN.get_perdict())
 
-    if draw_state:  
-        editor(rects, CurrentImage)
-    pygame.draw.rect(SCREEN, (255,0,0), reset_button)
-    pygame.display.flip()
+#     if draw_state:  
+#         editor(rects, CurrentImage)
+#     pygame.draw.rect(SCREEN, (255,0,0), reset_button)
+#     pygame.display.flip()
